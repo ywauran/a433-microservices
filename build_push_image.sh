349f553a078b2c3f2054f62885861d6cd9cdace5
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-06-02 06:43:01 +0800

    change file build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Perintah untuk membuat Docker image dari Dockerfile dengan nama image item-app dan tag v1
-docker build -t ywauran/item-app:v1 .
+docker build -t item-app:v1 .
 
 # Melihat daftar image di lokal
 docker images
 
 # Mengubah nama image agar sesuai dengan format Docker Hub
-docker tag item-app:v1 ywauran/item-app:v1
+docker tag item-app:v1 item-app:v1
 
 # Login ke Docker Hub
 docker login
 
 # Mengunggah image ke Docker Hub
-docker push ywauran/item-app:v1
+docker push item-app:v1
